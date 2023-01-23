a1729a356f7399e6b0e4369bc83487a38cea8b06
MaxorV <andmxv@gmail.com>
2023-01-23 01:08:45 -0300

    habit-tracker full application

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { prisma } from './lib/prisma'
+import { appRoutes } from './routes'
+
+const app = Fastify()
+
+app.register(cors)
+/**
+ * Método HTTP: Get, Post, Put, Patch, Delete
+ */
+app.register(appRoutes)
+
+app.listen({
+	port: 3333,
+	host: "0.0.0.0",
+}).then(() => {
+	console.log('HTTP server running!')
+})
